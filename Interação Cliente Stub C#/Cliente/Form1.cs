9f6fefde430093455ff8ff2f3a13b6166e9b0f70
Supreme Developer <unconfigured@null.spigotmc.org>
2019-01-09 17:37:14 -0200

    Interação Cliente Stub C#
    Demonstrando a interação entre dois executáveis onde um faz alterações em outro sem utilizar registros ou arquivos externos.

---
+++ Interação Cliente Stub C#/Cliente/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Cliente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //Arquivo Esboço - Stub
+            string fnStub = Path.GetDirectoryName(Application.ExecutablePath) + @"\Stub.exe";
+                      
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "arquivo EXE|*.exe";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string fnSaida = sfd.FileName;
+
+                //Copia stub para local selecionado
+                File.Copy(fnStub, fnSaida);
+
+                string d = "[D]";
+                string saidaDados = "";
+
+                //Todos os dados que serão passados
+                string[] dados = new string[] { textBox1.Text };
+
+                foreach (string dado in dados)
+                {
+                    saidaDados += d + dado;
+                }
+
+                //Acrescentar dados
+                File.AppendAllText(fnSaida, saidaDados);
+            }
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Interação Cliente Stub C#/Stub/main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace stub
+{
+    class main
+    {
+
+        static void Main()
+        {
+
+            //Auto Leitura
+            byte[] byteArr = File.ReadAllBytes(Application.ExecutablePath);
+            string textoArquivo = Encoding.UTF8.GetString(byteArr);
+
+            //Secciona o código
+            string delimitador = "[D]";
+            string[] dados = textoArquivo.Split(new string[] { delimitador }, StringSplitOptions.None);
+            
+            //MessageBox
+            MessageBox.Show(dados[1]);
+
+        }
+
+    }
+}
